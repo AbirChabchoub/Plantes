f7301135717461e06a90dbbce33845d2cbf903e0
houssem eddin amri <amri.houcem.eddine@gmail.com>
2021-06-04 18:37:05 +0200

    houssem test pull

--- src/app/services/orders.service.ts
+++ src/app/services/orders.service.ts
@@ -2,19 +2,16 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class OrdersService {
+	constructor(private httpClient: HttpClient) {}
+	orderUrl = 'http://localhost:3001/orders';
 
-  constructor(private httpClient:HttpClient) { }
-  orderUrl = "http://localhost:3001/orders";
-
-
-addOrder(order:any){
-  this.httpClient.post<{message:string}>(this.orderUrl, order); 
-}
-getOrderByUserId(id:any){
-  this.httpClient.get<{order:any}>(`${this.orderUrl}/${id}`);
-}
-
+	addOrder(order: any) {
+		return this.httpClient.post<{ message: string }>(this.orderUrl, order);
+	}
+	getOrderByUserId(id: any) {
+		return this.httpClient.get<{ order: any }>(`${this.orderUrl}/${id}`);
+	}
 }
